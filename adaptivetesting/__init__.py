2d5739cfd5ff8719e10276ffcb38533496ce44a2
Jonas Engicht <dev@condecon.de>
2024-06-08 17:49:05 +0200

    update subpackage init.py

--- adaptivetesting/__init__.py
+++ adaptivetesting/__init__.py
@@ -1,12 +1,2 @@
 """This is a backend package for adaptive testing.
 Please, only import object from the toplevel."""
-
-from .models.test_item import TestItem, load_test_items_from_list
-from .math.ml_estimation import MLEstimator
-from .math.standard_error import standard_error
-from .math.test_information import test_information_function
-from .math.urrys_rule import urrys_rule
-from .models.adaptive_test import AdaptiveTest
-from .models.algorithm_exception import AlgorithmException
-from .models.test_item import TestItem
-from .models.test_result import TestResult
--- adaptivetesting/implementations/__init__.py
+++ adaptivetesting/implementations/__init__.py
@@ -1,4 +1,3 @@
-"""Subpackage for adaptive test implementations"""
 from .default_implementation import *
+from .pre_test import *
 from .semi_implementation import *
-from .pre_test import PreTest
