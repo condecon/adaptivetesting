1f8c015753728bf5f8bf7f4ef85a6ded4b60123a
Jonas Engicht <dev@condecon.de>
2024-06-08 17:54:30 +0200

    update mail in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import setuptools
 
 setuptools.setup(
     name="adaptivetesting",
-    version="0.0.1",
+    version="1.0.0",
     author="Jonas Engicht",
-    author_email="dev@condecon.de",
+    author_email="jonas.engicht@uni-jena.de",
     url="https://github.com/condecon/adaptivetesting",
     packages=["adaptivetesting", 
               "adaptivetesting.data",
