69776b33cfa47aeb8d5cbe70a99ce5f94931a845
Jonas Engicht <dev@condecon.de>
2024-10-25 14:33:26 +0200

    add description and maintainer

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setuptools.setup(
     version="1.0.0",
     author="Jonas Engicht",
     author_email="jonas.engicht@uni-jena.de",
+    maintainer="Jonas Engicht",
+    maintainer_email="jonas.engicht@uni-jena.de",
+    description="a Python package for Computerized Adaptive Testing",
+    long_description="""
+adaptivetesting is a Python package that can be used to simulate and evaluate CAT scenarios as well as implement them in real-world testing scenarios from a single codebase.
+""",
+    long_description_content_type="text",
     url="https://github.com/condecon/adaptivetesting",
     packages=["adaptivetesting", 
               "adaptivetesting.data",
