4e520f31cd2a2bb672646d58a2748a2f0c2d33e8
Jonas Engicht <dev@condecon.de>
2024-11-13 09:10:09 +0100

    conditional type conversion

--- adaptivetesting/services/__estimator_interface.py
+++ adaptivetesting/services/__estimator_interface.py
@@ -4,8 +4,8 @@ import numpy as np
 
 class IEstimator(ABC):
     def __init__(self,
-                 response_pattern: List[int],
-                 item_difficulties: List[float]):
+                 response_pattern: List[int] | np.ndarray,
+                 item_difficulties: List[float] | np.ndarray):
         """This is the interface required for every possible
         estimator.
         Any estimator inherits from this class and implements
@@ -15,8 +15,16 @@ class IEstimator(ABC):
             response_pattern (List[int]): list of responses (0: wrong, 1:right)
             item_difficulties (List[float]): list of item difficulties
         """
-        self.response_pattern = np.array(response_pattern)
-        self.item_difficulties = np.array(item_difficulties)
+        if type(response_pattern) is not np.ndarray:
+            self.response_pattern = np.array(response_pattern)
+        else:
+            self.response_pattern = response_pattern
+        
+        if type(item_difficulties) is not np.ndarray:
+            self.item_difficulties = np.array(item_difficulties)
+        else:
+            self.item_difficulties = item_difficulties
+        
 
     @abstractmethod
     def get_estimation(self) -> float:
