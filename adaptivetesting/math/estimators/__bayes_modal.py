c98d2ac91f49641c3a5889a4d31ecb6216a31093
Jonas Engicht <dev@condecon.de>
2025-01-04 15:31:10 +0100

    remove bayes modal

--- adaptivetesting/math/estimators/__bayes_modal.py
+++
@@ -1,9 +0,0 @@
-from ...services.__estimator_interface import IEstimator
-
-class BayesModal(IEstimator):
-    def __init__(self, response_pattern, item_difficulties, prior):
-        super().__init__(response_pattern, item_difficulties)
-
-
-    def get_estimation(self):
-        pass
