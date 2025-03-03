33e0f565c9a727346e19ebb36306069df908da63
codecon <dev@condecon.de>
2025-03-03 10:14:15 +0100

    add 4pl testinformation test

--- adaptivetesting/tests/test_information_test.py
+++ adaptivetesting/tests/test_information_test.py
@@ -1,7 +1,7 @@
 import unittest
 from adaptivetesting.math import test_information_function
 import jax.numpy as np
-
+import pandas as pd
 
 class TestInformation(unittest.TestCase):
     def test_information_calculation_1pl(self):
@@ -19,4 +19,17 @@ class TestInformation(unittest.TestCase):
         self.assertAlmostEqual(result, 0.6559974211, 3)
 
     def test_information_calculation_4pl(self):
-        pass
+        items = pd.DataFrame({
+            "a": [1.32, 1.07, 0.84],
+            "b": [-0.63, 0.18, -0.84],
+            "c": [0.17, 0.10, 0.19],
+            "d": [0.87, 0.93, 1]
+        })
+
+        result = test_information_function(0,
+                                           items.a.to_numpy(),
+                                           items.b.to_numpy(),
+                                           items.c.to_numpy(),
+                                           items.d.to_numpy())
+        
+        # print(1 / np.sqrt(result))
