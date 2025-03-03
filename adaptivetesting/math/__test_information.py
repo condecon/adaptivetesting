fix standard error and item information calculation

adaptivetesting/math/__test_information.py
@@ -1,8 +1,10 @@
 import jax.numpy as np
 from .estimators.__functions.__estimators import probability_y1, probability_y0
-from jax import jit
+from jax import jit, grad
+from scipy.differentiate import derivative
 
-@jit
+
+
 def test_information_function(
             mu: np.ndarray,
                a: np.ndarray, 
@@ -25,10 +27,16 @@ def test_information_function(
 
     """
     p_y1 = probability_y1(mu, a, b, c, d)
-    p_y0 = probability_y0(mu, a, b, c, d)
+    
+    p_y1_grad = derivative(probability_y1, 
+                           mu,
+                           args=(a, b, c, d))
+    
+    
 
-    product = p_y0 * p_y1
+    product = (p_y1_grad.df ** 2) / (p_y1 * (1 - p_y1))
     information_tensor = np.cumsum(product)
+    # print(f"Information tensor: {information_tensor}")
     information = information_tensor[len(information_tensor) - 1]
     # convert information to float and return
     return information.astype(float)

adaptivetesting/tests/test_information_test.py
@@ -31,5 +31,7 @@ class TestInformation(unittest.TestCase):
                                            items.b.to_numpy(),
                                            items.c.to_numpy(),
                                            items.d.to_numpy())
-        
-        # print(1 / np.sqrt(result))
+        # convert test information into standard error
+        result = 1 / np.sqrt(result)
+
+        self.assertAlmostEqual(result, 1.444873, 3)