98f303ae41102335f7cc39edd6ba83ea20707dfc
codecon <dev@condecon.de>
2025-03-14 10:18:42 +0100

    implement standard error for bayes

--- adaptivetesting/math/__standard_error.py
+++ adaptivetesting/math/__standard_error.py
@@ -1,17 +1,26 @@
 import math
-from typing import List
+from typing import List, Literal
 import jax.numpy as np
 from .__test_information import test_information_function
 from ..models.__test_item import TestItem
 
 
-def standard_error(answered_items: List[TestItem], estimated_ability_level: float) -> float:
+def standard_error(answered_items: List[TestItem],
+                   estimated_ability_level: float,
+                   estimator: Literal["ML", "BM"] = "ML",
+                   sd: float | None = None) -> float:
     """Calculates the standard error using the test information function.
+    If Bayes Modal is used for the ability estimation, a standard deviation value
+    of the prior distribution has to be provided.
 
     Args:
         answered_items (List[float]): List of answered items
 
         estimated_ability_level (float): Currently estimated ability level
+
+        estimator (Literal["ML", "BM"]): Ability estimator (Default: ML)
+
+        sd (float | None): Standard deviation of the prior distribution. Only required for BM.
 
     Returns:
         float: Standard error
@@ -21,11 +30,24 @@ def standard_error(answered_items: List[TestItem], estimated_ability_level: floa
     c=np.array([item.c for item in answered_items])
     d=np.array([item.d for item in answered_items])
 
-    error = 1 / math.sqrt(test_information_function(mu=np.array(estimated_ability_level),
-                                                    a=a,
-                                                    b=b,
-                                                    c=c,
-                                                    d=d
-                                                    ))
+    if estimator == "ML":
+        error = 1 / math.sqrt(test_information_function(mu=np.array(estimated_ability_level),
+                                                        a=a,
+                                                        b=b,
+                                                        c=c,
+                                                        d=d
+                                                        ))
 
-    return error
+        return error
+    
+    if estimator == "BM":
+        information = test_information_function(mu=np.array(estimated_ability_level),
+                                                        a=a,
+                                                        b=b,
+                                                        c=c,
+                                                        d=d)
+        error = 1 / math.sqrt(
+            (1 / math.pow(sd, 2)) + information
+        )
+
+        return error
