64af67ff439d57e3b741fab4830c568734f8ebee
condecon <dev@condecon.de>
2025-05-18 16:29:46 +0200

    add tests for wrong prior inheritance

--- adaptivetesting/tests/test_bayes_modal.py
+++ adaptivetesting/tests/test_bayes_modal.py
@@ -3,6 +3,7 @@ from adaptivetesting.models import ItemPool
 from adaptivetesting.math.estimators import BayesModal, NormalPrior, CustomPrior
 import pandas as pd
 from scipy.stats import beta
+from adaptivetesting.math.estimators import CustomPriorException
 
 
 class TestBayesModal(unittest.TestCase):
@@ -58,4 +59,29 @@ class TestCustomPrior(unittest.TestCase):
         )
 
         result = estimator.get_estimation()
-        print(result)
+        self.assertAlmostEqual(result, 0.01, places=3)
+
+    def test_wrong_prior_implementation(self):
+        items = pd.DataFrame({"a":[1.3024,1.078,0.8758,0.5571,1.225,0.991,0.9968,1.1888,1.1642,1.1188],
+                              "b":[-0.6265,0.1836,-0.8356,1.5953,0.3295,-0.8205,0.4874,0.7383,0.5758,-0.3054],
+                              "c":[0.2052,0.1618,0.1957,0.1383,0.1324,0.1973,0.0058,0.1193,0.1831,0.1732],
+                              "d":[0.8694,0.9653,0.8595,0.8112,0.7677,0.7749,0.8291,0.8797,0.9155,0.8517]})
+        item_pool = ItemPool.load_from_dataframe(items)
+
+        response_pattern = [0,0,1,1,1,0,0,0,0,1]
+
+        # create custom prior
+        class WrongPrior(NormalPrior):
+            pass
+
+        prior = WrongPrior(0, 1)
+        
+        estimator = BayesModal(
+            response_pattern=response_pattern,
+            items=item_pool.test_items,
+            prior=prior,
+            optimization_interval=(-10, 10)
+        )
+
+        with self.assertRaises(CustomPriorException):
+            estimator.get_estimation()
