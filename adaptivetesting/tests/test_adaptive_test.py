ff93d1c50a505c9c5f4138e1ce500ab25030ff6f
condecon <dev@condecon.de>
2025-06-10 12:03:49 +0200

    fix test for adaptive test

--- adaptivetesting/tests/test_adaptive_test.py
+++ adaptivetesting/tests/test_adaptive_test.py
@@ -1,6 +1,7 @@
 import unittest
 from adaptivetesting.models import AdaptiveTest, TestItem, ItemPool
 from adaptivetesting.math.estimators import MLEstimator
+from adaptivetesting.math.item_selection import urrys_rule
 
 item1 = TestItem()
 item1.b = 0.24
@@ -31,6 +32,9 @@ class TestAdaptiveTest(unittest.TestCase, AdaptiveTest):
             participant_id="0"
         )
         unittest.TestCase.__init__(self, methodName)
+    
+    def get_next_item(self):
+        return urrys_rule(self.item_pool.test_items, self.ability_level)
 
     def estimate_ability_level(self) -> tuple[float, float]:
         return 0, float("NaN")
@@ -48,13 +52,13 @@ class TestAdaptiveTest(unittest.TestCase, AdaptiveTest):
 
     def test_get_next_item(self):
         next_item = self.get_next_item()
-        self.assertEqual(next_item, item1)
+        self.assertDictEqual(next_item.as_dict(), item1.as_dict())
 
     def test_testing_procedure_once(self):
         self.run_test_once()
         # test showed item
         self.assertEqual(self.test_results[0].showed_item,
-                         item1.b)
+                         item1.as_dict())
 
         # print(self.test_results)
         # print(simulated_responses)
@@ -66,4 +70,4 @@ class TestAdaptiveTest(unittest.TestCase, AdaptiveTest):
 
         # test item is removed from pool
         # print(self.item_pool.test_items)
-        self.assertEqual(self.item_pool.test_items, [item2, item3])
+        self.assertListEqual([item.as_dict() for item in self.item_pool.test_items], [item2.as_dict(), item3.as_dict()])
