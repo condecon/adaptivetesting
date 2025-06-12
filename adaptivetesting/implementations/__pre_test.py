convert jax to numpy and fix gradient calculation

adaptivetesting/implementations/__pre_test.py
@@ -1,5 +1,5 @@
 from typing import List
-import jax.numpy as jnp
+import numpy as np
 from ..models.__test_item import TestItem
 import random
 
@@ -22,17 +22,17 @@ class PreTest:
         self.items = items
         self.seed = seed
 
-    def calculate_quantiles(self) -> jnp.ndarray:
+    def calculate_quantiles(self) -> np.ndarray:
         """Calculates quantiles 0.25, 0.5, 0.75
         """
         # get difficulties
         difficulties: List[float] = [item.b for item in self.items]
 
-        quantiles = jnp.array([])
+        quantiles = np.array([])
         # calculate quantiles
         for q in [0.25, 0.5, 0.75]:
-            quantile: jnp.ndarray = jnp.quantile(jnp.array(difficulties), q)
-            quantiles = jnp.append(quantiles, quantile)
+            quantile: np.ndarray = np.quantile(np.array(difficulties), q)
+            quantiles = np.append(quantiles, quantile)
 
         return quantiles
 
@@ -65,7 +65,7 @@ class PreTest:
         """
         selected_items: List[TestItem] = []
         # create difficulties array
-        difficulties = jnp.array([item.b for item in self.items])
+        difficulties = np.array([item.b for item in self.items])
         # add minimum and maximum difficulty values to
         # the quantiles list for correct item selection
         quantiles = [difficulties.min()]

adaptivetesting/math/estimators/__expect_a_posteriori.py
@@ -1,5 +1,5 @@
-import jax.numpy as jnp
-from jax.scipy.integrate import trapezoid
+import numpy as np
+from scipy.integrate import trapezoid
 from .__bayes_modal_estimation import BayesModal
 from ...models.__test_item import TestItem
 from .__functions.__bayes import likelihood
@@ -9,7 +9,7 @@ from math import pow
 
 class ExpectedAPosteriori(BayesModal):
     def __init__(self,
-                 response_pattern: list[int] | jnp.ndarray,
+                 response_pattern: list[int] | np.ndarray,
                  items: list[TestItem],
                  prior: Prior,
                  optimization_interval: tuple[float, float] = (-10, 10)):
@@ -20,7 +20,7 @@ class ExpectedAPosteriori(BayesModal):
             This type of estimation finds the mean of the posterior distribution.
 
             Args:
-                response_pattern (List[int] | jnp.ndarray): list of response patterns (0: wrong, 1:right)
+                response_pattern (List[int] | np.ndarray): list of response patterns (0: wrong, 1:right)
 
                 items (List[TestItem]): list of answered items
             
@@ -36,11 +36,11 @@ class ExpectedAPosteriori(BayesModal):
         Returns:
             float: ability estimation
         """
-        x = jnp.linspace(self.optimization_interval[0], self.optimization_interval[1], 1000)
+        x = np.linspace(self.optimization_interval[0], self.optimization_interval[1], 1000)
         
         prior_pdf = self.prior.pdf(x)
         
-        likelihood_vals = jnp.vectorize(lambda mu: likelihood(mu,
+        likelihood_vals = np.vectorize(lambda mu: likelihood(mu,
                                                               self.a,
                                                               self.b,
                                                               self.c,
@@ -71,10 +71,10 @@ class ExpectedAPosteriori(BayesModal):
         Returns:
             float: standard error of the ability estimation
         """
-        x = jnp.linspace(self.optimization_interval[0], self.optimization_interval[1], 1000)
+        x = np.linspace(self.optimization_interval[0], self.optimization_interval[1], 1000)
         prior_pdf = self.prior.pdf(x)
         
-        likelihood_vals = jnp.vectorize(lambda mu: likelihood(mu,
+        likelihood_vals = np.vectorize(lambda mu: likelihood(mu,
                                                               self.a,
                                                               self.b,
                                                               self.c,

adaptivetesting/math/item_selection/__maximum_information_criterion.py
@@ -2,7 +2,7 @@ from ...models.__test_item import TestItem
 from ...models.__item_selection_exception import ItemSelectionException
 from ..estimators.__test_information import item_information_function
 from ...models.__algorithm_exception import AlgorithmException
-import jax.numpy as jnp
+import numpy as np
 
 
 def maximum_information_criterion(items: list[TestItem],
@@ -26,15 +26,15 @@ def maximum_information_criterion(items: list[TestItem],
 
     for item in items:
         # extract parameters from the current item
-        a = jnp.array([item.a])
-        b = jnp.array([item.b])
-        c = jnp.array([item.c])
-        d = jnp.array([item.d])
+        a = np.array([item.a])
+        b = np.array([item.b])
+        c = np.array([item.c])
+        d = np.array([item.d])
 
         # calculate information for the current item
         try:
             information = float(item_information_function(
-                mu=jnp.array(ability, dtype=float),
+                mu=np.array(ability, dtype=float),
                 a=a,
                 b=b,
                 c=c,

adaptivetesting/services/__estimator_interface.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple
-import jax.numpy as jnp
+import numpy as np
 from ..models.__test_item import TestItem
 
 
 class IEstimator(ABC):
     def __init__(self,
-                 response_pattern: List[int] | jnp.ndarray,
+                 response_pattern: List[int] | np.ndarray,
                  items: List[TestItem],
                  optimization_interval: Tuple[float, float] = (-10, 10)):
         """This is the interface required for every possible
@@ -19,17 +19,17 @@ class IEstimator(ABC):
             
             items (List[TestItem]): list of answered items
         """
-        if type(response_pattern) is not jnp.ndarray:
-            self.response_pattern = jnp.array(response_pattern)
+        if type(response_pattern) is not np.ndarray:
+            self.response_pattern = np.array(response_pattern)
         else:
             self.response_pattern = response_pattern
         self.optimization_interval = optimization_interval
 
         # convert items to parameter arrays
-        self.a = jnp.array([i.a for i in items])
-        self.b = jnp.array([i.b for i in items])
-        self.c = jnp.array([i.c for i in items])
-        self.d = jnp.array([i.d for i in items])
+        self.a = np.array([i.a for i in items])
+        self.b = np.array([i.b for i in items])
+        self.c = np.array([i.c for i in items])
+        self.d = np.array([i.d for i in items])
         
     @abstractmethod
     def get_estimation(self) -> float: