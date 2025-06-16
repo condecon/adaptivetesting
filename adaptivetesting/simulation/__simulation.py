717445e36390d6dc9413c585cdbf483be386220b
codecon <dev@condecon.de>
2025-06-16 11:35:44 +0200

    fix platform detection

--- adaptivetesting/simulation/__simulation.py
+++ adaptivetesting/simulation/__simulation.py
@@ -140,7 +140,7 @@ class SimulationPool():
         # check for platform
         # this is because multiprocessing is not as well supported on windows
         # therefore, multithreading is used instead
-        if platform.system == "Windows":
+        if platform.system() == "Windows":
             with ThreadPoolExecutor(max_workers=60) as executor:
                 futures = [executor.submit(func, test) for test in self.adaptive_tests]
                 for _ in tqdm(as_completed(futures), total=len(futures)):
