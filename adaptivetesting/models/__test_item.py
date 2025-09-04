5a1b094f2bc4184ecd7caa4e64270a5cfcac273f
Tim Schäfer <tim.schaefer@ae.mpg.de>
2025-09-04 14:31:04 +0200

    Add new unit tests for loading TestItems with IDs

--- adaptivetesting/models/__test_item.py
+++ adaptivetesting/models/__test_item.py
@@ -4,10 +4,11 @@ class TestItem:
         The format is equal to the implementation in catR.
 
         Properties:
-            - a (float):
-            - b (float): difficulty
-            - c (float):
-            - d (float):
+            - id (int | None): item ID
+            - a (float): discrimination parameter
+            - b (float): difficulty parameter
+            - c (float): guessing parameter
+            - d (float): slipping parameter / upper asymptote
 
         """
         self.id: int | None = None
@@ -15,11 +16,18 @@ class TestItem:
         self.b: float = float("nan")
         self.c: float = 0
         self.d: float = 1
-    
-    def as_dict(self) -> dict[str, float]:
-        return {
+
+    def as_dict(self, with_id: bool = False) -> dict[str, float]:
+        """Returns the item as a dictionary"""
+
+        item_dict: dict[str, float] = {
             "a": self.a,
             "b": self.b,
             "c": self.c,
             "d": self.d
         }
+
+        if with_id and self.id is not None:
+            item_dict["id"] = self.id
+
+        return item_dict
