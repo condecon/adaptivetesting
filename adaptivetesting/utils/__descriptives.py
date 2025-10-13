1ad788183d0258cffdc0556b1d289972ee1a8bf9
codecon <dev@condecon.de>
2025-10-13 08:16:15 +0200

    progress

---
+++ adaptivetesting/utils/__descriptives.py
@@ -0,0 +1,13 @@
+import glob
+
+
+
+def bias() -> float:
+    ...
+
+def average_absolute_deviation() -> float:
+    ...
+
+
+def rmse() -> float:
+    ...
---
+++ adaptivetesting/utils/__plots.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+
+def plot_final_ability_estimates() -> tuple[Figure, Axes]:
+    fig, ax = plt.subplots()
+
+    return (fig, ax)
