29e0f113912759c4418c40bfc6ff2fd3758e943a
condecon <dev@condecon.de>
2025-11-18 16:42:07 +0100

    refactor: enhance numerical stability in get_standard_error method

--- adaptivetesting/math/estimators/__expect_a_posteriori.py
+++ adaptivetesting/math/estimators/__expect_a_posteriori.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.integrate import trapezoid
 from .__bayes_modal_estimation import BayesModal
 from ...models.__test_item import TestItem
-from .__functions.__estimators import likelihood, log_likelihood
+from .__functions.__estimators import log_likelihood
 from .__prior import Prior
 from math import pow
 
@@ -85,18 +85,26 @@ class ExpectedAPosteriori(BayesModal):
             float: standard error of the ability estimation
         """
         x = np.linspace(self.optimization_interval[0], self.optimization_interval[1], 1000)
-        prior_pdf = self.prior.pdf(x)
         
-        likelihood_vals = np.vectorize(lambda mu: likelihood(mu,
-                                                             self.a,
-                                                             self.b,
-                                                             self.c,
-                                                             self.d,
-                                                             self.response_pattern))(x)
+        # get log-prior in a numerically stable way
+        if hasattr(self.prior, "logpdf"):
+            log_prior = self.prior.logpdf(x)
+        else:
+            log_prior = np.log(self.prior.pdf(x) + 1e-300)
         
-        numerator = trapezoid((x - estimated_ability) ** 2 * likelihood_vals * prior_pdf, x)
+        log_likelihood_vals = np.vectorize(lambda mu: log_likelihood(mu,
+                                                                     self.a,
+                                                                     self.b,
+                                                                     self.c,
+                                                                     self.d,
+                                                                     self.response_pattern))(x)
+        log_posterior = log_likelihood_vals + log_prior
+        max_log = np.nanmax(log_posterior)
+        weights = np.exp(log_posterior - max_log)
+
+        numerator = trapezoid((x - estimated_ability) ** 2 * weights, x)
         
-        denominator = trapezoid(likelihood_vals * prior_pdf, x)
+        denominator = trapezoid(weights, x)
         
         standard_error_result = pow(numerator / denominator, 0.5)
     
