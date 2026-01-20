a212cd4ff48c7387a28747696f84afee48b7bfe0
Jonas Engicht <dev@condecon.de>
2026-01-20 13:40:38 +0100

    Revert "add BaseItemPool and PolyItemPool classes with loading methods"
    This reverts commit 9f9df2b5e0353eac19cd8b75a375d7e1c4b41e7d.

--- adaptivetesting/models/__item_pool.py
+++ adaptivetesting/models/__item_pool.py
@@ -1,12 +1,9 @@
-
-from .__test_item import TestItem, BaseItem
-from .__base_item_pool import BaseItemPool
+from .__test_item import TestItem
 from typing import List, Tuple
 from pandas import DataFrame
 
 
-
-class ItemPool(BaseItemPool):
+class ItemPool:
     def __init__(self,
                  test_items: List[TestItem],
                  simulated_responses: List[int] | None = None):
@@ -18,20 +15,88 @@ class ItemPool(BaseItemPool):
 
         Args:
             test_items (List[TestItem]): A list of test items. Necessary for any adaptive test.
-            simulated_responses (List[int]): A list of simulated responses.
-                Required for CAT simulations.
-        """
-        super().__init__(test_items, simulated_responses)
 
+            simulated_responses (List[int]): A list of simulated responses.
+            Required for CAT simulations.
+        """
+        self.test_items: List[TestItem] = test_items
+        self.simulated_responses: List[int] | None = simulated_responses
+
+    def get_item_by_index(self, index: int) -> Tuple[TestItem, int] | TestItem:
+        """Returns item and if defined the simulated response.
+
+        Args:
+            index (int): Index of the test item in the item pool to return.
+
+        Returns:
+            TestItem or (TestItem, Simulated Response)
+        """
+        selected_item = self.test_items[index]
+        if self.simulated_responses is not None:
+            simulated_response = self.simulated_responses[index]
+            return selected_item, simulated_response
+        else:
+            return selected_item
+
+    def get_item_by_item(self, item: TestItem) -> Tuple[TestItem, int] | TestItem:
+        """Returns item and if defined the simulated response.
+
+        Args:
+            item (TestItem): item to return.
+
+        Returns:
+            TestItem or (TestItem, Simulated Response)
+        """
+        index = self.test_items.index(item)
+        selected_item = self.test_items[index]
+        if self.simulated_responses is not None:
+            simulated_response = self.simulated_responses[index]
+            return selected_item, simulated_response
+        else:
+            return selected_item
+
+    def get_item_response(self, item: TestItem) -> int:
+        """
+        Gets the simulated response to an item if available.
+        A `ValueError` will be raised if a simulated response is not available.
+
+        Args:
+            item (TestItem): item to get the corresponding response
+
+        Returns:
+            (int): response (either `0` or `1`)
+        """
+        if self.simulated_responses is None:
+            raise ValueError("Simulated responses not provided")
+        else:
+            i, res = self.get_item_by_item(item) # type: ignore
+            return res
+
+    def delete_item(self, item: TestItem) -> None:
+        """Deletes item from item pool.
+        If simulated responses are defined, they will be deleted as well.
+
+        Args:
+            item (TestItem): The test item to delete.
+        """
+        # get index
+        index = self.test_items.index(item)
+        # remove item at index
+        self.test_items.pop(index)
+        # remove response at index
+        if self.simulated_responses is not None:
+            self.simulated_responses.pop(index)
+
+# STATIC LOAD METHODS
     @staticmethod
     def load_from_list(
-        b: List[float],
-        a: List[float] | None = None,
-        c: List[float] | None = None,
-        d: List[float] | None = None,
-        simulated_responses: List[int] | None = None,
-        ids: List[int] | None = None,
-        content_categories: list[list[str]] | None = None) -> "ItemPool":
+            b: List[float],
+            a: List[float] | None = None,
+            c: List[float] | None = None,
+            d: List[float] | None = None,
+            simulated_responses: List[int] | None = None,
+            ids: List[int] | None = None,
+            content_categories: list[list[str]] | None = None) -> "ItemPool":
         """
         Creates test items from a list of floats.
 
@@ -50,6 +115,7 @@ class ItemPool(BaseItemPool):
 
         """
         items: List[TestItem] = []
+
         for difficulty in b:
             item = TestItem()
             item.b = difficulty
@@ -88,6 +154,7 @@ class ItemPool(BaseItemPool):
 
         item_pool = ItemPool(items)
         item_pool.simulated_responses = simulated_responses
+
         return item_pool
 
     @staticmethod
@@ -152,12 +219,17 @@ class ItemPool(BaseItemPool):
             item.b = b[i]
             item.c = c[i]
             item.d = d[i]
+
             if ids is not None:
                 item.id = ids[i]
+
             if content_categories is not None:
                 item.additional_properties["category"] = content_categories[i]
+
             items.append(item)
+
         item_pool = ItemPool(items, simulated_responses)
+
         return item_pool
 
     @staticmethod
@@ -211,4 +283,5 @@ class ItemPool(BaseItemPool):
         if "simulated_responses" in source.columns:
             simulated_responses: List[int] = source["simulated_responses"].values.tolist() # type: ignore
             item_pool.simulated_responses = simulated_responses
+
         return item_pool
